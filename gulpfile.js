f297534975c735dac67c6bd15e62b4d479a0df2d
Rostuslav Kuzemsky <r.kuzemsky@gmail.com>
2017-01-19 15:54:41 +0200

    dropdown menu

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@ var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var rename = require('gulp-rename');
 var minifyCss = require('gulp-clean-css');
+var prefix = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
  
 gulp.task('css', function () {
   gulp.src('scss/style.scss')
     .pipe(sass())
+    .pipe(prefix())
     .pipe(minifyCss())
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('css/'));
 });
 
 gulp.task('watch', function () {
-  	gulp.watch('scss/*.scss', ['css'])
+  	gulp.watch('css/style.min.css', ['css'])
 });
 
 gulp.task('default', ['css', 'watch']);
